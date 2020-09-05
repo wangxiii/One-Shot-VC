4fa677e45e8de52a854aed8065da37f3348409b2
EricTtTtT <erictien02@gmail.com>
2020-09-05 11:29:39 +0800

    change path

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-config', '-c', default='config.yaml')
     parser.add_argument('-data_dir', '-d', 
-            default='/storage/feature/LibriTTS/sr_24000_mel_norm')
+            default='/work/b07u1234/b07505026/hw2-2/vctk/trimmed_vctk_spectrograms/sr_24000_mel_norm')
     parser.add_argument('-train_set', default='train')
-    parser.add_argument('-train_index_file', default='train_samples_64.json')
+    parser.add_argument('-train_index_file', default='train_samples_128.json')
     parser.add_argument('-logdir', default='log/')
     parser.add_argument('--load_model', action='store_true')
     parser.add_argument('--load_opt', action='store_true')
-    parser.add_argument('-store_model_path', default='/storage/model/adaptive_vc/model')
-    parser.add_argument('-load_model_path', default='/storage/model/adaptive_vc/model')
+    parser.add_argument('-store_model_path', default='/work/b07u1234/b07505026/hw2-2/model')
+    parser.add_argument('-load_model_path', default='/work/b07u1234/b07505026/hw2-2/model')
     parser.add_argument('-summary_steps', default=100, type=int)
     parser.add_argument('-save_steps', default=5000, type=int)
     parser.add_argument('-tag', '-t', default='init')
-    parser.add_argument('-iters', default=0, type=int)
+    parser.add_argument('-iters', default=50000, type=int)
 
     args = parser.parse_args()
     
